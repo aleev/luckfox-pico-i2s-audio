ef5ed3cfca9fd44a1266c26bebefdbe7c4a91068
Vladimir Aleev <aleyev@gmail.com>
2024-02-27 12:28:39 +0300

    Add files via upload

--- sysdrv/cfg/package.mk
+++ sysdrv/cfg/package.mk
@@ -5,12 +5,12 @@ $(eval $(call MACRO_CHECK_ENABLE_PKG, RK_ENABLE_EUDEV))
 
 
 # Enable build adbd debug tool
-CONFIG_SYSDRV_ENABLE_ADBD=y
+CONFIG_SYSDRV_ENABLE_ADBD=n
 $(eval $(call MACRO_CHECK_ENABLE_PKG, RK_ENABLE_ADBD))
 
 
 # Enable build gdb and gdbserver debug tool
-CONFIG_SYSDRV_ENABLE_GDB=y
+CONFIG_SYSDRV_ENABLE_GDB=n
 $(eval $(call MACRO_CHECK_ENABLE_PKG, RK_ENABLE_GDB))
 
 
